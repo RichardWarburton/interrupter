9890a568f79f3de9d18e6880ea617bc4d058f0f9
Richard Warburton <richard.warburton@gmail.com>
2009-06-25 06:28:28 +0100

    added lgpl licensing to the library

--- src/interrupter/MethodInterrupter.java
+++ src/interrupter/MethodInterrupter.java
@@ -1,3 +1,20 @@
+/*
+    Copyright 2009 Richard Warburton (richard.warburton@gmail.com)
+    This file is part of Interrupter.
+
+    Interrupter is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Interrupter is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with Interrupter.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package interrupter;
 import static org.objectweb.asm.Opcodes.INVOKESTATIC;
 import static org.objectweb.asm.Opcodes.RETURN;
